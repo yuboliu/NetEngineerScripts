c1396c04cdd05e95626de892f5c3e1f4a6487cca
Yu-bo Liu <txyu78356@gmail.com>
2020-11-22 18:36:53 +0800

    添加GTM_Oper_VS.py

---
+++ F5/Py/GTM/GTMSettings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+"""
+@Author: 刘钰博
+@Contact: lolipop.boy@outlook.com
+@Time: 2020/11/22 10:32
+"""
+
+import sys
+import os
+sys.path.append(os.path.abspath("../"))
+import lib.ConfigSettings
+
+
+if __name__ == "__main__":
+    pass
+
